b3eef42109e0e1feeaf6cd825b931306628f8978
meredith <meredithbrown302@gmail.com>
2022-04-29 15:34:07 -0400

    added post-text.tsx and post-text.scss (NOT CURRENTLY FUNCTIONAL)

---
+++ src/main/webapp/app/modules/post/post-text.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const BlogTextInput = () => {
+  const [text, onChangeText] = React.useState("Blog Text");
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+  height: 40,
+  margin: 12,
+  borderWidth: 1,
+  padding: 10,
+  },
+});
+
+export default BlogTextInput;
