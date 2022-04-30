36002a7e26a16c6e44c31e0385f3ff22f2683f30
meredith <meredithbrown302@gmail.com>
2022-04-30 13:15:44 -0400

    cleaned up post.tsx

--- src/main/webapp/app/modules/post/post.tsx
+++ src/main/webapp/app/modules/post/post.tsx
@@ -27,40 +27,6 @@ export const Post = () => {
         <h3>Text</h3>
         <h3>Import Images</h3>
         <h3>Tags</h3>
-        <p className="lead">
-          <Translate contentKey="post.subtitle">This is your postpage</Translate>
-        </p>
-        {account?.login ? (
-          <div>
-            <Alert color="success">
-              <Translate contentKey="post.logged.message" interpolate={{ username: account.login }}>
-                You are logged in as user {account.login}.
-              </Translate>
-            </Alert>
-          </div>
-        ) : (
-          <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
-
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </div>
-        )}
       </Col>
       <Col md="5" className="pad">
         <div className="blog-text-add">
