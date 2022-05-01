64bc1ed5994a99becb83f502dc361613e85d747d
ubuntu_x64_20_04_2022 <davidsamcun@gmail.com>
2022-05-01 15:55:31 -0400

    save

--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -15,6 +15,7 @@ import PrivateRoute from 'app/shared/auth/private-route';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import PageNotFound from 'app/shared/error/page-not-found';
 import { AUTHORITIES } from 'app/config/constants';
+import MyBlogs from './modules/myblogs/myblogs';
 
 const loading = <div>loading ...</div>;
 
@@ -41,6 +42,7 @@ const Routes = () => {
         <PrivateRoute path="/admin" component={Admin} hasAnyAuthorities={[AUTHORITIES.ADMIN]} />
         <PrivateRoute path="/account" component={Account} hasAnyAuthorities={[AUTHORITIES.ADMIN, AUTHORITIES.USER]} />
         <ErrorBoundaryRoute path="/post" exact component={Post} />
+        <ErrorBoundaryRoute path="/myblogs" exact component={MyBlogs} />
         <ErrorBoundaryRoute path="/" exact component={Home} />
         <PrivateRoute path="/" component={EntitiesRoutes} hasAnyAuthorities={[AUTHORITIES.USER]} />
         <ErrorBoundaryRoute component={PageNotFound} />
