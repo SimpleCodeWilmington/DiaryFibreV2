a4790b27e3dc9594fe635748fb2c41ea00cb7880
Miles Coney <MilesEConey@gmail.com>
2022-05-01 23:20:51 -0400

    Updated the blog-comment, and added a button on the post page to leave a comment

--- src/main/webapp/app/modules/post/post.tsx
+++ src/main/webapp/app/modules/post/post.tsx
@@ -1,13 +1,12 @@
 import './post.scss';
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
 import { Row, Col, Alert, Button } from 'reactstrap';
 
 import { useAppSelector } from 'app/config/store';
-
 import { BlogTextAdd } from 'app/entities/blog-text/blog-text-add';
+import AddComment from '../comments/add-comment';
 
 export const Post = () => {
   const account = useAppSelector(state => state.authentication.account);
@@ -17,10 +16,10 @@ export const Post = () => {
       <Col md="6">
         <h2>This page is for making blog posts!</h2>
         <Link to="/post">
-            <Button type="button">Simple</Button>
+          <Button type="button">Simple</Button>
         </Link>
         <Link to="/post-advanced">
-            <Button type="button">Advanced</Button>
+          <Button type="button">Advanced</Button>
         </Link>
         <h3>Template</h3>
         <h3>Title</h3>
@@ -31,6 +30,9 @@ export const Post = () => {
       <Col md="6" className="pad">
         <div className="blog-text-add">
           <BlogTextAdd />
+        </div>
+        <div className="add-comment">
+          <AddComment />
         </div>
       </Col>
     </Row>
