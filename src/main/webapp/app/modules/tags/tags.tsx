db09386ca8c693ed8269d2bbffd92a5e9fbe885c
meredith <meredithbrown302@gmail.com>
2022-05-02 22:07:47 -0400

    tags component - partial progress

--- src/main/webapp/app/modules/tags/tags.tsx
+++ src/main/webapp/app/modules/tags/tags.tsx
@@ -1,22 +1,52 @@
 import './tags.scss';
 
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Translate } from 'react-jhipster';
-import { Row, Col, Alert, Button } from 'reactstrap';
 import { useAppSelector } from 'app/config/store';
 
 export const Tags = () => {
   const account = useAppSelector(state => state.authentication.account);
+  const [input, setInput] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  const onKeyDown = (e) => {
+    const { key } = e;
+    const trimmedInput = input.trim();
+
+    if (key === 'Enter' && trimmedInput.length && !tags.includes(trimmedInput)) {
+        e.preventDefault();
+        setTags(prevState => [...prevState, trimmedInput]);
+        setInput('');
+    }
+  };
 
   return (
-    <div class="wrapper">
-      <input type="text" id="tags" autocomplete="off">
-      </input>
-      <div class="tag-container">
-      </div>
+    <div className="container">
+    {tags.map((tag) => <div className="tag">{tag}</div>)}
+    <input
+      value={input}
+      placeholder="Tags"
+      onKeyDown={onKeyDown}
+      onChange={onChange}
+    >
+    </input>
     </div>
   );
 };
 
 export default Tags;
+
+// resource: https://blog.logrocket.com/building-a-tag-input-field-component-for-react/
+
+
+
+
+
+
+
+
+
