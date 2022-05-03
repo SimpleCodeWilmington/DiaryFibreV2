f507e14711fc7f8cd18df605c15d31ee6e5ac97b
ubuntu_x64_20_04_2022 <davidsamcun@gmail.com>
2022-05-02 20:43:56 -0400

    added library

--- src/main/webapp/app/modules/myblogs/myblogs.tsx
+++ src/main/webapp/app/modules/myblogs/myblogs.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
 import { Row, Col, Alert } from 'reactstrap';
 
+
 import { useAppSelector } from 'app/config/store';
 
 export const MyBlogs = () => {
