b913dbc8e03b621f9e2bedadff83cca1d365d33b
Miles Coney <MilesEConey@gmail.com>
2022-05-03 20:44:22 -0400

    Updated

--- src/main/webapp/app/entities/blog-comment/blog-comment.tsx
+++ src/main/webapp/app/entities/blog-comment/blog-comment.tsx
@@ -4,7 +4,7 @@ import { Button, Table } from 'reactstrap';
 import { Translate, TextFormat } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
+import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT} from 'app/config/constants';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 
 import { IBlogComment } from 'app/shared/model/blog-comment.model';
@@ -28,51 +28,22 @@ export const BlogComment = (props: RouteComponentProps<{ url: string }>) => {
 
   return (
     <div>
-      <h2 id="blog-comment-heading" data-cy="BlogCommentHeading">
-        <Translate contentKey="diaryFibreApp.blogComment.home.title">Blog Comments</Translate>
         <div className="d-flex justify-content-end">
-          <Button className="me-2" color="info" onClick={handleSyncList} disabled={loading}>
-            <FontAwesomeIcon icon="sync" spin={loading} />{' '}
-            <Translate contentKey="diaryFibreApp.blogComment.home.refreshListLabel">Refresh List</Translate>
-          </Button>
           <Link to="/blog-comment/new" className="btn btn-primary jh-create-entity" id="jh-create-entity" data-cy="entityCreateButton">
             <FontAwesomeIcon icon="plus" />
             &nbsp;
             <Translate contentKey="diaryFibreApp.blogComment.home.createLabel">Create new Blog Comment</Translate>
           </Link>
         </div>
-      </h2>
       <div className="table-responsive">
         {blogCommentList && blogCommentList.length > 0 ? (
           <Table responsive>
-            <thead>
-              <tr>
-                <th>
-                  <Translate contentKey="diaryFibreApp.blogComment.id">ID</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="diaryFibreApp.blogComment.comment">Comment</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="diaryFibreApp.blogComment.dateTime">Date Time</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="diaryFibreApp.blogComment.blog">Blog</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="diaryFibreApp.blogComment.user">User</Translate>
-                </th>
-                <th />
-              </tr>
-            </thead>
             <tbody>
               {blogCommentList.map((blogComment, i) => (
-                <tr key={`entity-${i}`} data-cy="entityTable">
-                  <td>
-                    <Button tag={Link} to={`/blog-comment/${blogComment.id}`} color="link" size="sm">
-                      {blogComment.id}
-                    </Button>
-                  </td>
+              
+                <tr key={`entity-${i}`} data-cy="entityTable"> 
+                {/* this is what makes it a table remove when ready */}
+                  
                   <td>{blogComment.comment}</td>
                   <td>{blogComment.dateTime ? <TextFormat type="date" value={blogComment.dateTime} format={APP_DATE_FORMAT} /> : null}</td>
                   <td>{blogComment.blog ? <Link to={`/blog-post/${blogComment.blog.id}`}>{blogComment.blog.id}</Link> : ''}</td>
@@ -106,6 +77,7 @@ export const BlogComment = (props: RouteComponentProps<{ url: string }>) => {
                     </div>
                   </td>
                 </tr>
+                
               ))}
             </tbody>
           </Table>
