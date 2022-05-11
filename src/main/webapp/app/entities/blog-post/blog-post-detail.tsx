683a4234a336418b955861f7b7427d07fdaee3c7
meredith <meredithbrown302@gmail.com>
2022-05-11 15:52:41 -0400

    formatting on blog-post-detail.tsx and blog-post-detail.scss

--- src/main/webapp/app/entities/blog-post/blog-post-detail.tsx
+++ src/main/webapp/app/entities/blog-post/blog-post-detail.tsx
@@ -21,53 +21,47 @@ export const BlogPostDetail = (props: RouteComponentProps<{ id: string }>) => {
   const blogPostEntity = useAppSelector(state => state.blogPost.entity);
   
   return (
-    <Row>
-      <Col md="2"></Col>
-      <Col md="8">
-        <div className="blog-post-and-comments">
+    <Col md="8" className="column">
+      <div className="blog-post-and-comments">
 
-          <div className="date-time">
-            <dd>{blogPostEntity.dateTime ? <TextFormat value={blogPostEntity.dateTime} type="date" format={POST_DATE_FORMAT} /> : null}</dd>
+        <div className="date-time">
+          <dd>{blogPostEntity.dateTime ? <TextFormat value={blogPostEntity.dateTime} type="date" format={POST_DATE_FORMAT} /> : null}</dd>
+        </div>
+
+        <dl className="jh-entity-details">
+
+          <div className="post-title">
+            <dd>{blogPostEntity.title}</dd>
           </div>
 
-          <dl className="jh-entity-details">
-
-            <div className="post-title">
-              <dd>{blogPostEntity.title}</dd>
-            </div>
-
-            <div {...props} className="post-image">
-              <img src="content/images/icecream.jpg" className="centered" />
-            </div>
+          <div {...props} className="post-image">
+            <img src="content/images/icecream.jpg" className="centered" />
+          </div>
 
           <div className="post-text">
             <dd>{blogPostEntity.text}</dd>
           </div>
 
 
-          <dt>
-            <Translate contentKey="diaryFibreApp.blogPost.blog">Blog</Translate>
-          </dt>
-          <dd>{blogPostEntity.blog ? blogPostEntity.blog.id : ''}</dd>
-          <dt>
-            <Translate contentKey="diaryFibreApp.blogPost.tag">Tag</Translate>
-          </dt>
-          <dd>
-            {blogPostEntity.tags
-              ? blogPostEntity.tags.map((val, i) => (
-                  <span key={val.tagName}>
-                    <a>{val.tagName}</a>
-                    {blogPostEntity.tags && i === blogPostEntity.tags.length - 1 ? '' : ', '}
-                  </span>
-                ))
-              : null}
-          </dd>
-        </dl>
-        <Comments />
-        </div>
-      </Col>
-      <Col md="2"></Col>
-    </Row>
+    <dt>
+      <Translate contentKey="diaryFibreApp.blogPost.blog">Blog</Translate>
+    </dt>
+    <dd>{blogPostEntity.blog ? blogPostEntity.blog.id : ''}</dd>
+
+        <dd>
+          {blogPostEntity.tags
+            ? blogPostEntity.tags.map((val, i) => (
+                <span key={val.tagName}>
+                  <a className="tag">{val.tagName}</a>
+                  {blogPostEntity.tags && i === blogPostEntity.tags.length - 1 ? '' : ' '}
+                </span>
+              ))
+            : null}
+        </dd>
+  </dl>
+  <Comments />
+    </div>
+  </Col>
   );
 };
 
